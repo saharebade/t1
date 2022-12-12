4e0539cd6ec53d2fe3e385c66ea151bc4c6f4760
saharebade <120370811+saharebade@users.noreply.github.com>
2022-12-12 12:42:22 +0330

    Add files via upload

---
+++ SaharEbadi.py
@@ -0,0 +1,15 @@
+x="hi how are you friend"
+print(x)
+num_h=len(x.split(" "))
+num_k=len(x)
+a=x.replace('r','p')
+print("num_h=",num_h)
+print("Replace=",a)
+print()
+
+
+
+q="hi how are you ? , I am fine., whats up?, the health."
+num_j=len(q.split(","))
+print(q)
+print("num_j",num_j)
